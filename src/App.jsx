a5cbe78fd6a32dde34c05ed7d8fab73654219186
Kiran Nepali <itsmekiran44@gmai.com>
2024-02-17 19:35:46 +0545

    m

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,36 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import { LocomotiveScrollProvider } from "react-locomotive-scroll";
-import { useRef } from "react";
-import "./locomotive.css";
+// import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+// import { useRef } from "react";
+// import "./locomotive.css";
 export default function App() {
-  const containerRef = useRef(null);
+  // const containerRef = useRef(null);
 
   return (
-    <LocomotiveScrollProvider
-      options={{
-        smooth: true,
-      }}
-      watch={[]}
-      containerRef={containerRef}
-    >
-      <main className="bg-[#201e2a]" data-scroll-container ref={containerRef}>
-        <Navbar />
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
-      </main>
-    </LocomotiveScrollProvider>
+    // <LocomotiveScrollProvider
+    //   options={{
+    //     smooth: true,
+    //   }}
+    //   watch={[]}
+    //   containerRef={containerRef}
+    // >
+    //   <main className="bg-[#201e2a]" data-scroll-container ref={containerRef}>
+    //     <Navbar data-scroll-sticky />
+    //     <Hero />
+    //     <About />
+    //     <Skills />
+    //     <Projects />
+    //     <Contact />
+    //   </main>
+    // </LocomotiveScrollProvider>
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
   );
 }
